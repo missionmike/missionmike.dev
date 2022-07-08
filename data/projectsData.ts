cf1bebbfdc61ecd31158200610500377df91632e
Michael Dinerstein <md@missionmike.dev>
2022-07-07 23:09:11 -0700

    Add projects and update author details.

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,19 +1,27 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'TRD Pro by The Real Deal',
+    description: `TRD Pro is a data intelligence platform tailored to real estate insights for
+    New York City and other markets. My involvement is in full stack development using WordPress,
+    PHP, Next.js (React+TypeScript), PostgreSQL, and Docker.`,
+    imgSrc: '/static/images/projects/trd-pro.jpg',
+    href: 'https://therealdeal.com/pro',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Recording Connection Website',
+    description: `Website design and development for recordingconnection.com. 
+    The site was converted from WordPress (theme + plugins) to a headless GatsbyJS + WordPress
+    installation. I designed, developed, and handled technical aspects of this website launch.`,
+    imgSrc: '/static/images/projects/recordingconnection.jpg',
+    href: 'https://www.recordingconnection.com',
+  },
+  {
+    title: 'RRFC Student Portal',
+    description: `The RRFC Student Portal was designed and built to accommodate hundreds
+    of students and teachers in remote-learning and hybrid environments. I spearheaded the concept,
+    design, and full-stack development of this project (WordPress, PHP, JavaScript, Salesforce).`,
+    imgSrc: '/static/images/projects/student-portal.jpg',
+    href: 'https://vimeo.com/285858970',
   },
 ]
 
