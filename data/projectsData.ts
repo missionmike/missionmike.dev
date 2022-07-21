4c9c0d53bb2b6da72e738e0ba0522eeb73972c4d
Michael Dinerstein <md@missionmike.dev>
2022-07-20 23:13:53 -0700

    Update content.

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -2,7 +2,7 @@ const projectsData = [
   {
     title: 'TRD Pro by The Real Deal',
     description: `TRD Pro is a data intelligence platform tailored to real estate insights for
-    New York City and other markets. My involvement is in full stack development using WordPress,
+    New York City and other major markets. My involvement is in full stack development using WordPress,
     PHP, Next.js (React+TypeScript), PostgreSQL, and Docker.`,
     imgSrc: '/static/images/projects/trd-pro.jpg',
     href: 'https://therealdeal.com/pro',
@@ -10,15 +10,15 @@ const projectsData = [
   {
     title: 'Recording Connection Website',
     description: `Website design and development for recordingconnection.com. 
-    The site was converted from WordPress (theme + plugins) to a headless GatsbyJS + WordPress
-    installation. I designed, developed, and handled technical aspects of this website launch.`,
+    The site was converted from WordPress (theme + plugins) to a headless WordPress + GatsbyJS
+    installation. I designed, developed, and handled technical aspects of the launch.`,
     imgSrc: '/static/images/projects/recordingconnection.jpg',
     href: 'https://www.recordingconnection.com',
   },
   {
     title: 'RRFC Student Portal',
-    description: `The RRFC Student Portal was designed and built to accommodate hundreds
-    of students and teachers in remote-learning and hybrid environments. I spearheaded the concept,
+    description: `The RRFC Student Portal was designed and built to accommodate students and 
+    teachers in remote-learning and hybrid environments. I spearheaded the concept,
     design, and full-stack development of this project (WordPress, PHP, JavaScript, Salesforce).`,
     imgSrc: '/static/images/projects/student-portal.jpg',
     href: 'https://vimeo.com/285858970',
