2401d0a51ac50d1ac6da46eb26d27a8a6411834e
Michael Dinerstein <md@missionmike.dev>
2022-09-24 18:07:43 -0700

    update: add blog content and fix jest/ts config.

---
+++ data/siteMeta.ts
@@ -0,0 +1,21 @@
+const siteMetadata = {
+  title: 'missionmike.dev Blog',
+  author: 'Michael R. Dinerstein',
+  headerTitle: 'missionmike.dev',
+  description: "A collection of Michael's musings.",
+  language: 'en-us',
+  theme: 'system', // system, dark or light
+  siteUrl: 'https://www.missionmike.dev/',
+  siteRepo: 'https://github.com/missionmike/missionmike.dev',
+  siteLogo: '/static/images/missionmike-profile.jpeg',
+  image: '/static/images/missionmike-profile.jpeg',
+  socialBanner: '/static/images/twitter-card.png',
+  email: 'holler@missionmike.dev',
+  github: 'https://github.com/missionmike',
+  twitter: 'https://twitter.com/missionmikedev',
+  facebook: 'https://facebook.com/missionmike',
+  youtube: 'https://www.youtube.com/channel/UC3m6NxN3ZhCpYkL4LM8gHMg',
+  linkedin: 'https://www.linkedin.com/in/michaeldinerstein',
+};
+
+export { siteMetadata };
--- jest.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  moduleDirectories: ['node_modules', __dirname],
-};
