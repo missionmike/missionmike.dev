faaa1604b3a5d4c470d17cb4f6ebd6e27d52c2d6
Michael Dinerstein <md@missionmike.dev>
2023-01-11 22:43:12 -0800

    update: post content

--- components/MDXComponents/MDXComponents.tsx
+++ components/MDXComponents/MDXComponents.tsx
@@ -8,6 +8,8 @@ const MDXComponents = {
   h1: (props) => <h1 {...props} className={styles.postTitle} />,
   h2: (props) => <h2 {...props} className={styles.postHeading2} />,
   h3: (props) => <h3 {...props} className={styles.postHeading3} />,
+  h4: (props) => <h3 {...props} className={styles.postHeading4} />,
+  Aside: (props) => <aside {...props} className={styles.aside} />,
   CodeHeader: (props) => <CodeHeader {...props} />,
   CodeBlock: (props) => <CodeBlock {...props} />,
 };
